b5d29f9e0708406558e9bdfe12997450875fcc94
Cassandra Geitner <nordicblue5345@gmail.com>
2023-03-13 05:00:08 -0700

    my arrow function

--- 011-functions/main.js
+++ 011-functions/main.js
@@ -9,12 +9,12 @@ function doMath (a, b) { a=10, b=4
    // console.log(`this is the result ${a + b}`)
 }
 
-doMoreMath = (a, b) =>
+doMoreMath = (a,b) => a * b
 
-
-
-console.log (doMoreMath(20, 30))
-
+console.log(doMoreMath(20, 30))
 
 const result = doMath(10, 4)
-console.log(`result was returned from the function ${result}`)
+console.log(`this result was returned from the function ${result}`)
+
+math = (a, b) => a + b
+    console.log(math(15,30))
