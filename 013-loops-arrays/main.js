26dbf65ed8db3afa0215ad1f6dac530ee1d2ccd2
Cassandra Geitner <nordicblue5345@gmail.com>
2023-04-11 22:21:33 -0700

    writing the average MPG using for loop

--- 013-loops-arrays/main.js
+++ 013-loops-arrays/main.js
@@ -1,6 +1,7 @@
 //Update the DOM
 
-const myArr = []
+const myMPG = []
+const myTripCost = []
 
 const updateDOM = (input) => {
     const divEl = document.getElementById('output')
@@ -11,19 +12,26 @@ const updateDOM = (input) => {
 
 const trackMPGCost= (miles, gallons, price = 3.79) => {
     const MPG = Math.round(miles/gallons)
-    const tripCost = gallons * price
+    const tripCost = Math.round(gallons * price)
     updateDOM(`Mileage is ${MPG} and trip cost is ${tripCost}`) 
-    myArr.push(MPG, tripCost)
+    myMPG.push(MPG)
+    myTripCost.push(tripCost)
 }
 
 const calculateAvg = () => {
-    const avgMPG =(myArr [0] + myArr [2]) / 2 
-    updateDOM(` Average Miles per gallon is ${avgMPG}`) 
+    //const avgMPG =(myArr [0] + myArr [2]) / 2 
+    //updateDOM(` Average Miles per gallon is ${avgMPG}`)
+     for (let i = 0; i < 10; i++) 
+        console.log(i)
 }
 
 
 
 trackMPGCost(360, 15, 5.40)
+trackMPGCost(320, 12, 5)
+trackMPGCost(500, 11, 3.50)
+trackMPGCost(320, 12, 5)
+trackMPGCost(50, 5, 2.40)
 trackMPGCost(320, 12, 5)
 //updateDOM(myArr)
 
