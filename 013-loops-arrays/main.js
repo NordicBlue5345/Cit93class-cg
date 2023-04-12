2dd420fcc9c778d6be9f420aaf9b7bc6c6718fd4
Cassandra Geitner <nordicblue5345@gmail.com>
2023-04-12 00:58:15 -0700

    my code for Avg cost. I got stuck at the end

--- 013-loops-arrays/main.js
+++ 013-loops-arrays/main.js
@@ -4,13 +4,13 @@ const myMPG = []
 const myTripCost = []
 
 const updateDOM = (input) => {
-    const divEl = document.getElementById('output')
+    const divEl = document.querySelector('#output')
     const p = document.createElement('p')
     p.textContent = input
     divEl.appendChild(p)
 }
 
-const trackMPGCost= (miles, gallons, price = 3.79) => {
+const trackMPGandCost= (miles, gallons, price = 3.79) => {
     const MPG = Math.round(miles/gallons)
     const tripCost = Math.round(gallons * price)
     updateDOM(`Mileage is ${MPG} and trip cost is ${tripCost}`) 
@@ -18,21 +18,30 @@ const trackMPGCost= (miles, gallons, price = 3.79) => {
     myTripCost.push(tripCost)
 }
 
-const calculateAvg = () => {
-    //const avgMPG =(myArr [0] + myArr [2]) / 2 
-    //updateDOM(` Average Miles per gallon is ${avgMPG}`)
-     for (let i = 0; i < 10; i++) 
-        console.log(i)
-}
+const calculateMPGAvg = () => {
+    let totalMPG = 0
+    for(let i = 0; i < myMPG.length; i++) {
+        totalMPG = totalMPG * myMPG[i]
+    }
+    let avgMPG  = totalMPG/myMPG.length
+    updateDOM (`Average MPG is ${avgMPG}`)
+    }
 
+const calculateAvgCost = () => {
+    let totalcost = 0
+    for(let i = 0; i < totalCost.length; i++) {
+            totalcost = totalCost + myCost[i]
+    }
+    let avgCost = totalcost/myCost.length
+    updateDOM (`Average Cost is ${avgCost}`)
 
+    }
+//calculateAvgCost
 
-trackMPGCost(360, 15, 5.40)
-trackMPGCost(320, 12, 5)
-trackMPGCost(500, 11, 3.50)
-trackMPGCost(320, 12, 5)
-trackMPGCost(50, 5, 2.40)
-trackMPGCost(320, 12, 5)
-//updateDOM(myArr)
-
-calculateAvg()
+trackMPGandCost(360, 15, 5.40)
+trackMPGandCost(320, 12, 5)
+trackMPGandCost(500, 11, 3.50)
+trackMPGandCost(320, 12, 5)
+trackMPGandCost(50, 5, 2.40)
+trackMPGandCost(320, 12, 5)
+calculateAvgCost()
