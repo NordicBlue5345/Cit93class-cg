ce28caa00c3872f2b0025f4f2a31212fcc5c2b1d
Cassandra Geitner <nordicblue5345@gmail.com>
2023-04-13 01:49:03 -0700

    my form

---
+++ 015-user-input/015-playing-with-forms/main.js
@@ -0,0 +1,60 @@
+const FORM = document.getElementById('form-input')
+
+const MY_MPG = []
+const MY_TRIP_COST = []
+
+const updateDOM = (input) => {
+    const divEl = document. querySelector('#output')
+    const p = document.createElement('p')
+    p.textContent = input
+    divEl.appendChild(p)
+}
+
+const trackMPGandCost = (miles, gallons, price = 3.79) => {
+    const MPG = Math.round(miles/gallons)
+    const tripCost = Math.round(gallons * price)
+    updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`)
+    MY_MPG.push(MPG)
+    MY_TRIP_COST.push(tripCost)
+}
+
+const calculateSUM = (arr) => {
+    let SUM = 0
+      //   for (let i =0; i < arr.length; i++) {
+    //     sum = sum + arr[i]
+    // }
+    // arr.forEach(Element => {
+    //      sum += Element
+    // })
+    for (value of arr) {
+        sum += value
+    }
+    return sum
+}
+
+const calculateAvg = () => {
+    let sumMPG = calculateSUM(MY_MPG)
+    let sumTripCost = calculateSUM(MY_TRIP_COST)
+    let avgMPG = Math.round(sumMPG / MY_MPG.length)
+    let avgTripCost = Math.round(sumTripCost / MY_TRIP_COST.length)
+    updateDOM(`Average MPG is ${avgMPG}`)
+    updateDOM(`Average Trip is ${avgTripCost}`)
+}
+
+FORM.addEventListener('submit', (e) => {
+    e.preventDefault
+    const travelTime = parseInt(e.target.travelTime.value)
+    const miles = parseInt(e.target.miles.value)
+    const price = parseInt(e.target.price.value)
+    tracktravelandCost(travelTime, miles, price)
+})
+
+//calculateAvgCost
+
+// trackMPGandCost(360, 15, 5.40)
+// trackMPGandCost(320, 12, 5)
+// trackMPGandCost(500, 11, 3.50)
+// trackMPGandCost(320, 12, 5)
+// trackMPGandCost(50, 5, 2.40)
+// trackMPGandCost(320, 12, 5)
+// calculateAvg()
