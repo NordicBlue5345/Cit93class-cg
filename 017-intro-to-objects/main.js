9ec15661212ba2ed11c3806b1210dec4e152dbca
Cassandra Geitner <nordicblue5345@gmail.com>
2023-04-14 01:16:43 -0700

    this is mine

--- 017-intro-to-objects/main.js
+++ 017-intro-to-objects/main.js
@@ -3,20 +3,31 @@ const myArr = []
 // console.log(typeof myObj)
 // console.log(typeof myArr)
 // console.log(myArry == myObj)
-const myNumber = 9
-const myObj = {
-    name: 'Cassandra',
-    age: 29,
-    hobbies: ['readings', 'drawing','singing'],
+const myNumber = 1645
+const myCountry = {
+    name: 'Wonderland',
+    year: '1645',
+    size: 500,
     mobile: {
-        make: 'pixel 7',
+        make: 'samsung 21',
         OS: 'android'
     },
-    myGreeting: function() {
-
-        return `${this.name} says What is UP ${person}`
+    myGreeting: function(history) {
+        return this.name + ' ' + this.year + ' ' + this. size;
+        call(Update) + this.myGreeting `This is the town of Wonderland. Famously named after the storybook tale`
     }
 }
 
-const greeting = myObj.myGreeting('Jane')
-console.log(greeting)
+const towns = [];
+   towns[0] ="MadHatter",
+   towns[1] ="RedQueen",
+   towns[3] ="WhiteKing",
+   towns[4] ="Jabberwoki",
+   towns[5] ="Alice",
+
+   (towns == "MadHatter")
+        //block off code to be executed if the condition is true
+        greeting = " This is the town MadHatter"
+
+const Update = history.year
+console.log(Update)
