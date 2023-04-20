f4ea493ea4f867c2c131d888bb7762630a2035b0
Cassandra Geitner <nordicblue5345@gmail.com>
2023-04-20 02:56:03 -0700

    fixing Code

--- 018-obj-min-globals-refactor/main.js
+++ 018-obj-min-globals-refactor/main.js
@@ -1,6 +1,6 @@
 const FORM = document.getElementById('form-input')
 const ERR = document.getElementById('err')
-const CALCULATE_AVG = document.getElementById('calculateAvg')
+const CALCULATE_AVG = document.getElementById('output-avg')
 
 const trackingObj = {
     myMPG: [],
@@ -10,54 +10,41 @@ const trackingObj = {
         const p = document.createElement('p')
         p.textContent = input
         divEl.appendChild(p)
-    }
-}
-
-const trackMPGandCost = (miles, gallons, price = 3.79) => {
+    },
+    trackMPGandCost: (miles, gallons, price = 3.79) => {
     const MPG = Math.round(miles/gallons)
     const tripCost = Math.round(gallons * price)
     this.updateDOM (`Miles per gallon is ${MPG} and trip cost is ${tripCost}`, '#output')
     this.myMPG.push(MPG)
     this.myTripCost.push(tripCost)
-}
-
-const calculateSUM = (arr) => {
+    },
+    calculateSUM: (arr) => {
     let SUM = 0
     for (value of arr) {
         SUM += value
     }
     return SUM
-}
-calculateAvg = {
-    const sumMPG = math.round(this.calculateSUM(this.myMPG),
-    const sumTripCost = Math.round(this.calculateSUM(this.myTripCost)),
-    const avgMPG = Math.round(sumMPG / MY_MPG.length)
-    let avgTripCost = Math.round(sumTripCost / MY_TRIP_COST.length)
+    },
+    calculateAvg: function () {
+    const sumMPG = this.calculateSUM(this.myMPG)
+    const sumTripCost = this.calculateSUM(this.myTripCost)
+    const avgMPG = Math.round(sumMPG / this.myMPG.length)
+    let avgTripCost = Math.round(sumTripCost/this.myTripCost.length)
     this.updateDOM(`Average MPG is ${avgMPG}`, '#output-avg')
     this.updateDOM(`Average Trip is ${avgTripCost}`, '#output-avg')
-}
+    },
 
-FORM.addEventListener('submit', (e) => {
-    e.preventDefault()
+    ValidateFORM: function (e) {
     const errMsg = []
     const miles = parseInt(e.target.miles.value)
     const gallon = parseInt(e.target.gallons.value)
     const price = parseInt(e.target.price.value)
     if (miles === 0 || gallons === 0 || price === 0) {
         errMsg.push('Make sure you input value greater than zero. Try Again! ')
-        setTimeout(() => {
-            console.log("Delayed for 1 second.");
-          }, 3000);
-    } 
-
+    }
     if(price > 1000) {
         errMsg.push('Really!!!?? I think this is in error...Try again')
-
-        setTimeout(() => {
-            console.log("Delayed for 1 second.");
-          }, 3000);
     }
-        
     if(errMsg.length > 0) {
         ERR.textContent = errMsg
     } else {
@@ -67,4 +54,5 @@ FORM.addEventListener('submit', (e) => {
         calculateAvg()
     }   
     FORM.reset()
-})
+    }
+}
