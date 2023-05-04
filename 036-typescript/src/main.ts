47be9a7ac5ad49b5c79fcfc83b8747d4623b5db4
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-04 09:22:42 -0700

    finish basic types

--- 036-typescript/src/main.ts
+++ 036-typescript/src/main.ts
@@ -7,4 +7,14 @@ let album: any;
 myName = 'John'
 meaningOfLife = 42
 isLoading = true
-album= 1984
+album= 5150
+
+const sum = (a: number, b: string) => {
+    return a + b
+}
+
+
+let postId: string | number
+let isActive: number| boolean
+
+let re: RegExp = /\w +/g
