4f284b079405881b5fe581b0e6708f38bf433d34
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-04 06:17:17 -0700

    Updated

--- 004-let-typeof/main.js
+++ 004-let-typeof/main.js
@@ -2,6 +2,6 @@ let name = 'Cassandra'
 let age = 29
 let isAge = age === 29
 
-console.log(name)
-console.log(age)
-console.log(isAge)
+console.log(typeof Cassandra)
+console.log(typeof 29)
+console.log(typeof age === 29)
