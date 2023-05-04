583a739c3d77af6e35802abe6c6fbb543f0265f2
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-04 09:14:47 -0700

    More on types

--- 036-typescript/src/main.ts
+++ 036-typescript/src/main.ts
@@ -1,11 +1,10 @@
-let username = 'Dave';
-console.log(username);
-
-let test = 'one two';
-let a = 12;
-let b = 6;
-let c = 2;
+let myName: string = 'Dave'
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
 
 
-console.log(a / b);
-console.log(c * b);
+myName = 'John'
+meaningOfLife = 42
+isLoading = true
+album= 1984
