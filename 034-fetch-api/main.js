d05b29ea35bf4d4bbe69c2be10815e1661bb64e3
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-04 03:12:28 -0700

    Fetch Header Object

--- 034-fetch-api/main.js
+++ 034-fetch-api/main.js
@@ -1,10 +1,23 @@
 const url =
-'http://api.thenewsapi.com/v1/news/top? api_token=' 
+'http://api.spotify.com/v1/artist/0k17h0d3j5vfsdmq1izte9' 
 
-async function getData() {
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data)
+const request = new Request(url, {
+    headers: { 
+        'Authorization': 'Bearer 123'
+    }
+})
+async function getData() {    
+    try {
+        const response = await fetch(request)
+        const data = await response.json()
+        if(response.status === 200) {
+        console.log('Success', data)
+        }else {
+            console.log('Server error', data.error.message)
+        }
+    } catch (error) {
+        console.log('Error', error)
+    }
 }
 
-getData()
+getData
