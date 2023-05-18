c48f523d5ed29cf6dfd2b65b93fcbf73b7b87ce9
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-18 02:17:16 -0700

    more on vriables

---
+++ 010-typeof/scripts/main.js
@@ -0,0 +1,24 @@
+let myName = "Cass"
+let myAge = 29
+let myHappiness = true
+let newVar = 'something'
+const myConst = 42
+console.log(myAge == myName)
+console.log(myAge === myName)
+
+let myVar = null
+console.log('myVar is typeof ${typeof myVar}')
+
+console.log(`myName is typeof ${typeof myName}`)
+console.log(`myName is value of ${myName}`)
+console.log(`myAge is typeof ${typeof myAge}`)
+console.log(`myHappiness is typeof ${typeof myHappiness}`)
+
+// dont use var 
+
+//if (true) {
+//    const myConst = 43
+//    console.log(myConst)
+//} else {
+
+//}
