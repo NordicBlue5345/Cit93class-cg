853580c2d2b3dfa34dfdf39f59d9dc9e813a6ae6
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-18 13:51:09 -0700

    my form input

--- 100-my-app/main.js
+++ 100-my-app/main.js
@@ -1,24 +1,63 @@
-// Global variables
-let stringValue = "Hello";
-let numberValue = 10;
-let booleanValue = true;
+// Define the global variables
+let globalString = "Hello";
+let globalNumber = 42;
+let globalBoolean = true;
 
-// Calculation function
-function performCalculation(a, b) {
-  return a + b;
+// Function to perform calculation
+function performCalculation(input1, input2) {
+  return input1 + input2;
 }
 
-// Decision making process based on inputs and conditions
-function makeDecision() {
-  if (stringValue === "Hello" && numberValue > 5) {
-    console.log("Condition 1: String value is 'Hello' and number value is greater than 5");
-  } else if (booleanValue || (numberValue < 0 && stringValue !== "Goodbye")) {
-    console.log("Condition 2: Boolean value is true or number value is negative and string value is not 'Goodbye'");
-  } else {
-    const result = performCalculation(numberValue, 5);
-    console.log(`Condition 3: Result of calculation is ${result}`);
+// Validate the form input
+function validateForm() {
+  // Get the form element and inputs
+  const formEl = document.getElementById('myForm');
+  const input1 = document.getElementById('input1');
+  const input2 = document.getElementById('input2');
+
+  // Get the values of the inputs
+  const value1 = input1.value;
+  const value2 = input2.value;
+
+  // Validate the inputs
+  if (value1 === '' || value2 === '') {
+    alert('Please fill in all fields.');
+    return;
   }
+
+  // Convert the inputs to numbers
+  const number1 = parseFloat(value1);
+  const number2 = parseFloat(value2);
+
+  // Check if the conversion was successful
+  if (isNaN(number1) || isNaN(number2)) {
+    alert('Please enter valid numbers.');
+    return;
+  }
+
+  // Call the function and get the result
+  const result = performCalculation(number1, number2);
+
+  // Create the object with data and decision
+  const dataObject = {
+    input1: number1,
+    input2: number2,
+    result: result,
+  };
+
+  // Push the object into an array
+  dataArray.push(dataObject);
+
+  // Reset the form
+  formEl.reset();
 }
 
-// Calling the decision making function
-makeDecision();
+// Array to store the data objects
+const dataArray = [];
+
+// Add event listener to the form submission
+const formEl = document.getElementById('myForm');
+formEl.addEventListener('submit', function (e) {
+  e.preventDefault();
+  validateForm();
+});
