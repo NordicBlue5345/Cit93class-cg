9104a7227097213c7adedf3e0f3672ac70c1b493
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-18 09:21:39 -0700

    already had working code

--- 027-intro-to-oop/README.md
+++ 027-intro-to-oop/README.md
@@ -1,1 +1,3 @@
 - I am understanding Class more as time goes on. I am understanding that it is a blueprint for objects or object like things and in a way a Class feels more personal than the others do. Im not sure if that can actually be a thing, but so far while working with the class it comes off as personal.
+
+-I ended up getting working code
--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -1,26 +1,62 @@
 class Person {
-    constructor(firstName, lastName, address){
-        this.fistName = firstName;
-        this.lastName = lastName;
-        this.address = address;
+    constructor(firstName, lastName, address) {
+      this._firstName = firstName;
+      this._lastName = lastName;
+      this._address = address;
     }
-    showStatus(){
-        console.log(`${this.fistName} lives at ${this.address}`)
+  
+    get firstName() {
+      return this._firstName;
     }
-}
-
-class Student extends Person {
-    constructor(fistName, lastName, address, classList) {
-        super(fistName, lastName, address)
-        this.classList = classList
+  
+    set firstName(newFirstName) {
+      this._firstName = newFirstName;
     }
-
-    showStatus(){
-        console.log(`${this.fistName} lives at ${this.address} is taking ${this.classList}`)
+  
+    get lastName() {
+      return this._lastName;
     }
-}
-
-const cass = new Person("Cass", "Franco", "123 no way i will share it", ["Eng1A", "CIT93"])
-cass.showStatus() 
-const nick = new Person("Nick", "Franco", "123 same house")
-nick.showStatus() 
+  
+    set lastName(newLastName) {
+      this._lastName = newLastName;
+    }
+  
+    get address() {
+      return this._address;
+    }
+  
+    set address(newAddress) {
+      this._address = newAddress;
+    }
+  
+    showStatus() {
+      console.log(`${this._firstName} ${this._lastName} lives at ${this._address}`);
+    }
+  }
+  
+  class Student extends Person {
+    constructor(firstName, lastName, address, classList) {
+      super(firstName, lastName, address);
+      this._classList = classList;
+    }
+  
+    get classList() {
+      return this._classList;
+    }
+  
+    set classList(newClassList) {
+      this._classList = newClassList;
+    }
+  
+    showStatus() {
+      console.log(`${this._firstName} ${this._lastName} lives at ${this._address} is taking ${this._classList}`);
+    }
+  }
+  
+  const cass = new Student("Cass", "Franco", "123 no way I will share it", ["Eng1A", "CIT93"]);
+  cass.showStatus();
+  
+  const nick = new Person("Nick", "Franco", "123 same house");
+  nick.showStatus();
+  
+  console.log(cass.firstName);
