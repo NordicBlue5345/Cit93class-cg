abb2fcae67695a3f76dbc78ff3ee5ee2bc1019ec
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-18 12:42:15 -0700

    workout refractor with promises

--- 030-asyn-workout/main.js
+++ 030-asyn-workout/main.js
@@ -1,24 +1,36 @@
-const formEl = document.getElementById('form')
-const output = document.getElementById('output')
+const formEl = document.getElementById('form');
+const output = document.getElementById('output');
 
 function updateDOM(message, el) {
-    const newEl = document.createElement(el)
-    newEl.textContent = message
-    output.appendChild(newEl)
+  return new Promise((resolve, reject) => {
+    const newEl = document.createElement(el);
+    newEl.textContent = message;
+    output.appendChild(newEl);
+    resolve();
+  });
 }
 
-function startWorkout (type, reps, time, fn) {
-    fn(`Start ${type} <> Goal reps is ${reps}`, 'p')
-    setTimeout(()=> {
-        fn(`Stop ${type}`, 'h1')
-    }, time * 1000)
+function startWorkout(type, reps, time, fn) {
+  fn(`Start ${type} <> Goal reps is ${reps}`, 'p')
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, time * 1000);
+      });
+    })
+    .then(() => {
+      fn(`Stop ${type}`, 'h1');
+    });
 }
 
-formEl.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const type = e.target.type.value
-    const reps = parseFloat(e.target.reps.value)
-    const time = parseFloat(e.target.time.value)
-    startWorkout(type, reps, time, updateDOM)
-    formEl.reset()
-})
+formEl.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const type = e.target.type.value;
+  const reps = parseFloat(e.target.reps.value);
+  const time = parseFloat(e.target.time.value);
+  startWorkout(type, reps, time, updateDOM)
+    .then(() => {
+      formEl.reset();
+    });
+});
