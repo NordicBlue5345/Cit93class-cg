912f361793e658f6bb37f5315238e0f045db140f
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-18 09:55:47 -0700

    Syn Code

---
+++ 029_async-code/main.js
@@ -0,0 +1,11 @@
+ let pizza 
+ function orderPizza() {
+    console.log('order pizza')
+    setTimeout(() => {
+        pizza = new
+    }, 2000)
+    console.log('Pizza was ordered')
+ }
+
+ orderPizza()
+ console.log('Eat ${pizza}')
