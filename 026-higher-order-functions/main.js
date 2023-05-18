2b823a7e87c4df3f60bff05b9232d5c42f96829a
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-18 06:18:28 -0700

    updated code

--- 026-higher-order-functions/main.js
+++ 026-higher-order-functions/main.js
@@ -5,7 +5,7 @@ import { isFormValid, calculateAvg, trackMPGandCost } from "./handleinput.js"
 
 const FORM = document.getElementById('form-input');
 const ERR = document.getElementById('err');
-const AVG_OUTPUT = document.getElementById('output-avg');
+
 
 
 /* MY_DATA is global array that will be updated by the user input with objects from form input values 
@@ -15,6 +15,7 @@ const MY_DATA = getTripData()
 
 renderTable(MY_DATA)
 
+tcFpKoeyvYW21jn2
 
 /* Eventlisteners for form submit button, checks validation and if valid saves input data and calculated 
 data as an object into global array named MY_DATA */
@@ -26,14 +27,12 @@ FORM.addEventListener('submit', (e) => {
     const price = parseFloat(e.target.price.value);
     const isValid = isFormValid(miles, gallons, price);
     if (isValid) {
-      ERR.textContent = '';
-      AVG_OUTPUT.textContent = ''; // Clear previous average
-      const dataObj = trackMPGandCost(miles, gallons, price);
-      MY_DATA.push(dataObj);
-      saveTripData(MY_DATA);
-      renderTable(MY_DATA, FORM);
-      const average = calculateAvg(MY_DATA); // Calculate average
-      AVG_OUTPUT.textContent = `Average: ${average}`; // Update average rendering
+        ERR.textContent = '';
+        AVG_OUTPUT.textContent = '';
+        const dataObj = trackMPGandCost(miles, gallons, price);
+        MY_DATA.push(dataObj);
+        saveTripData(MY_DATA)
+        renderTable(MY_DATA, FORM);
     }
     FORM.reset();
-  });
+});
