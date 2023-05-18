46c95bc5dfdd45151e13845bb952c166379a1075
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-18 13:08:40 -0700

    getting weather in Fresno

--- 031-promises/main.js
+++ 031-promises/main.js
@@ -1,14 +1,20 @@
 function fetchData() {
     return new Promise(function(resolve, reject){
-        fetch('https://api.weather,gov,gridpoints/OKX/35,35/forecast')
+        fetch('https://api.weather,gov,gridpoints/OKX/53,100/forecast')
     .then(response => response.json())
     .then(data => resolve(data.properties.periods[1].shortForecast));  
     })
 }  
 
 function displayData(weather) {
-    console.log('ERROR ${err}')
+    console.log(weather)
 }
+
+function oneError(err) {
+    console.log('Error: ${err}')
+}
+
+fetchData().then(displayData).catch(oneError)
 
 fetchData()
 .then(displayData)
