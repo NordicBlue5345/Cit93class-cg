0f9648395967c928522e2e0a41de74e764623e65
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-18 07:39:55 -0700

    fixing refractor #2

--- 020-refractor-arr-objs/main.js
+++ 020-refractor-arr-objs/main.js
@@ -1,60 +1,91 @@
-const FORM = document.getElementById('form-input');
-const ERR = document.getElementById('err');
-const AVG_OUTPUT = document.getElementById('output-avg');
+/* Global const for updating DOM elements by their id*/
 
-const MY_DATA = [];
+const FORM = document.getElementById('form-input')
+const ERR = document.getElementById('err')
+const AVG_OUTPUT = document.getElementById('output-avg')
+
+/* MY_DATA is global array that will be updated by the user input with objects from form input values 
+and calculate data */
+
+const MY_DATA = []
+
+/* updateDOM function takes in input (string value) and id (to determine DOM location to update) 
+and creates and updates DOM elements*/
 
 const updateDOM = (input, id) => {
-  const divEl = document.querySelector(id);
-  const p = document.createElement('p');
-  p.textContent = input;
-  divEl.appendChild(p);
-};
+    const divEl = document.querySelector(id)
+    const p = document.createElement('p')
+    p.textContent = input
+    divEl.appendChild(p)
+}
 
-const trackMPGandCost = (miles, gallons, price = 3.79) => {
-  const MPG = Math.round(miles / gallons);
-  const tripCost = Math.round(gallons * price);
-  updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`, '#output');
-  const dataObj = {
-    miles: miles,
-    gallons: gallons,
-    price: price,
-    MPG: MPG,
-    tripCost: tripCost
-  };
-  MY_DATA.push(dataObj);
-};
+/* trackMPGandCost function takes in miles, gallons and price and calculates MPG and tripCost and 
+returns an object */
 
-const calculateSUM = (arr) => {
-  let sum = 0;
-  for (value of arr) {
-    sum += value;
-  }
-  return sum;
-};
+const trackMPGandCost = (miles, gallons, price) => {
+    const MPG  = Math.round(miles/gallons)
+    const tripCost = Math.round(gallons * price)
+    updateDOM(`Miles per gallon  is ${MPG} and trip cost is ${tripCost}`, '#output')
+    return {
+        MPG: MPG, 
+        tripCost: tripCost,
+        miles: miles,
+        gallons: gallons,
+        price: price
+    }
+}
+
+/* calculateAvg function loops over the MY_DATA to determine average MPG and Trip Cost
+*/
 
 const calculateAvg = () => {
-    let sumMPG = calculateSUM(MY_MPG)
-    let sumTripCost = calculateSUM(MY_TRIP_COST)
-    let avgMPG = Math.round(sumMPG/MY_MPG.length)
-    let avgTripCost = Math.round(sumTripCost/MY_TRIP_COST.length)
+    const numberOfObj = MY_DATA.length
+    let sumMPG  = 0
+    let sumTripCost = 0  
+    MY_DATA.forEach(obj => {
+        sumMPG += obj.MPG
+        sumTripCost += obj.tripCost
+    })
+    const avgMPG = Math.round(sumMPG/numberOfObj)
+    const avgTripCost = Math.round(sumTripCost/numberOfObj)
     updateDOM(`Average MPG is ${avgMPG}`, '#output-avg')
     updateDOM(`Average Trip Cost is ${avgTripCost}`, '#output-avg')
 }
 
+/* isFormValid takes in miles, gallons and price and does simple validation and 
+returns boolean or truthy value back to eventlisteners */
+
+const isFormValid = (miles, gallons, price) => {
+    const errMsg = []
+    if (miles === 0 || gallons === 0 || price === 0) {
+        errMsg.push('Make sure your input value greater than 0!!, Try Again')
+    }
+    if (price > 1000) {
+        errMsg.push('Really!!!?? I think this is in error...Try again')
+    }
+    if (errMsg.length > 0) {
+        ERR.textContent = errMsg
+        return false
+    } else {
+        return true
+    }
+}
+
+/* Eventlisteners for form submit button, checks validation and if valid saves input data and calculated 
+data as an object into global array named MY_DATA */
 
 FORM.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const miles = parseInt(e.target.miles.value);
-    const gallons = parseInt(e.target.gallons.value);
-    const price = parseInt(e.target.price.value);
-    const isValid = isFormValid(miles, gallons, price);
-    if (isValid) {
-      ERR.textContent = '';
-      AVG_OUTPUT.textContent = '';
-      trackMPGandCost(miles, gallons, price);
-      renderTable();
-      calculateAvg();
+    e.preventDefault()
+    const miles = parseInt(e.target.miles.value)
+    const gallons = parseInt(e.target.gallons.value)
+    const price = parseInt(e.target.price.value)
+    const isValid = isFormValid(miles, gallons, price)
+    if(isValid) {
+        ERR.textContent = ''
+        AVG_OUTPUT.textContent = ''
+        const dataObj = trackMPGandCost(miles, gallons, price)
+        MY_DATA.push(dataObj)
+        calculateAvg()
     }
-    FORM.reset();
-  });
+    FORM.reset()  
+})
