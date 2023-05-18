599b8baf1e666bce0d5a27f98d3c5655d0980bf9
Cassandra Geitner <nordicblue5345@gmail.com>
2023-05-18 09:06:20 -0700

    human class

--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -9,6 +9,18 @@ class Person {
     }
 }
 
-const cass = new Person("Cass", "Franco", "123 no way i will share it")
+class Student extends Person {
+    constructor(fistName, lastName, address, classList) {
+        super(fistName, lastName, address)
+        this.classList = classList
+    }
+
+    showStatus(){
+        console.log(`${this.fistName} lives at ${this.address} is taking ${this.classList}`)
+    }
+}
+
+const cass = new Person("Cass", "Franco", "123 no way i will share it", ["Eng1A", "CIT93"])
 cass.showStatus() 
 const nick = new Person("Nick", "Franco", "123 same house")
+nick.showStatus() 
